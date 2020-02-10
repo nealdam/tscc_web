fc87783306306f095f17498a26caca10d40b0f2c
nealdam <neal.dam.1008@gmail.com>
2020-02-10 15:17:20 +0700

    private route template

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    useHistory,
-    useLocation
-} from "react-router-dom"
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import OperatorPage from "./pages/operator/OperatorPage";
 import ManagerPage from "./pages/manager/ManagerPage";
 import NoMatchPage from "./pages/noMatch/NoMatchPage";
 
 export default function App() {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/operator">
-                        <OperatorPage />
-                    </Route>
-                    <Route path="/manager">
-                        <ManagerPage />
-                    </Route>
-                    <Route exact path="/">
-                        <LoginPage />
-                    </Route>
-                    <Route path="*">
-                        <NoMatchPage />
-                    </Route>
-                </Switch>
-                
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <LoginPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/operator">
+            <OperatorPage />
+          </PrivateRoute>
+          <PrivateRoute path="/manager">
+            <ManagerPage />
+          </PrivateRoute>
+          <Route path="*">
+            <NoMatchPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const currentUser = {
+  isAuthenticated: false
+};
+
+function PrivateRoute({ children, ...rest }) {
+  //   const currentUser = authenticationService.currentUserValue;
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        currentUser.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
 }
