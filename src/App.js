387df18b0e7cabe68c41b5e3b57df272b2393f70
nealdam <neal.dam.1008@gmail.com>
2020-02-10 13:01:25 +0700

    web route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+} from "react-router-dom"
+import LoginPage from "./pages/login/LoginPage";
+import OperatorPage from "./pages/operator/OperatorPage";
+import ManagerPage from "./pages/manager/ManagerPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/operator">
+                        <OperatorPage />
+                    </Route>
+                    <Route path="/manager">
+                        <ManagerPage />
+                    </Route>
+                    <Route path="/">
+                        <LoginPage />
+                    </Route>
+                </Switch>
+                
+            </div>
+        </Router>
+    )
 }
-
-export default App;
