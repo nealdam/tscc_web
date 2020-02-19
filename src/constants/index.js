45cf3527dcae409d01ec964b05d4ac543a4c474c
nealdam <neal.dam.1008@gmail.com>
2020-02-19 16:17:16 +0700

    constant

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+//TODO: make this get from .env
+export const API_BASE_URL = 'localhost:5000';
+export const ACCESS_TOKEN = 'accessToken';
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 15;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
