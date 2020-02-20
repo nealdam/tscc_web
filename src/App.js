89c74d93b96fdb276fcfb9eb4026a623d6d0da87
nealdam <neal.dam.1008@gmail.com>
2020-02-20 15:55:40 +0700

    clean up

--- src/App.js
+++ src/App.js
@@ -11,8 +11,24 @@ import LoginPage from "./pages/login/LoginPage";
 import OperatorPage from "./pages/operator/OperatorPage";
 import ManagerPage from "./pages/manager/ManagerPage";
 import NoMatchPage from "./pages/noMatch/NoMatchPage";
+import firebase from 'firebase/app'
 
 export default function App() {
+  
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCPEyplo1MwwQwtomYxzaq_yQxymYD5AwU",
+      authDomain: "tscc-notification.firebaseapp.com",
+      databaseURL: "https://tscc-notification.firebaseio.com",
+      projectId: "tscc-notification",
+      storageBucket: "tscc-notification.appspot.com",
+      messagingSenderId: "951076084886",
+      appId: "1:951076084886:web:e5244ed835616b4859f6b7"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
   return (
     <Router>
       <div>
