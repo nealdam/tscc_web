a54e5ededb52927cf676991f3a17d9faabfdc52c
nealdam <neal.dam.1008@gmail.com>
2020-02-21 14:41:23 +0700

    implement login with email and password

--- src/organisms/loginForm/LoginForm.js
+++ src/organisms/loginForm/LoginForm.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, FormControlLabel, Checkbox, Button } from "@material-ui/core";
+import firebase from 'firebase';
+import { useSnackbar } from "notistack";
+import { successNotify, errorNotify } from "../../constants/notistackOption";
 
 export default function LoginForm() {
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(username, password)
+      .then(response => {
+        enqueueSnackbar("Login success", successNotify);
+        console.log(response);
+      })
+      .catch(error => {
+        enqueueSnackbar("Incorrect email or password", errorNotify);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode + errorMessage);
+      });
+  }
+
   return (
     <form>
       <TextField
@@ -14,6 +41,7 @@ export default function LoginForm() {
         name="username"
         autoComplete="username"
         autoFocus
+        onChange={(e) => setUsername(e.target.value)}
       />
       <TextField
         variant="outlined"
@@ -25,12 +53,13 @@ export default function LoginForm() {
         type="password"
         id="password"
         autoComplete="current-password"
+        onChange={(e) => setPassword(e.target.value)}
       />
       <FormControlLabel
         control={<Checkbox value="remember" color="primary" />}
         label="Remember me"
       />
-      <Button type="submit" fullWidth variant="contained" color="primary">
+      <Button type="submit" fullWidth variant="contained" color="primary" onClick={(e) => handleLogin(e)}>
         Login
       </Button>
     </form>
