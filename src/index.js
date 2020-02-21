e8ca8f6087b026bb1c497f25dc7a0141a62fde8a
nealdam <neal.dam.1008@gmail.com>
2020-02-21 12:23:08 +0700

    implement notistack

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { SnackbarProvider } from "notistack";
 
 // Register a service worker
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("./firebase-messaging-sw.js")
-      .then(function(registration) {
-        console.log("Registration successful, scope is:", registration.scope);
-      })
-      .catch(function(err) {
-        console.log("Service worker registration failed, error:", err);
-      });
-  }
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then(function(registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function(err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <SnackbarProvider>
+    <App />
+  </SnackbarProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
