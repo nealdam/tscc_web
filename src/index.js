53508dd214def572914c3fdc8ba6c70f9f80f37f
nealdam <neal.dam.1008@gmail.com>
2020-03-14 16:32:13 +0700

    remove unuse server worker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { SnackbarProvider } from "notistack";
 
 // Register a service worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("./firebase-messaging-sw.js")
-    .then(function(registration) {
+    .then(function (registration) {
       console.log("Registration successful, scope is:", registration.scope);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log("Service worker registration failed, error:", err);
     });
 }
@@ -26,8 +25,3 @@ ReactDOM.render(
   </SnackbarProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
