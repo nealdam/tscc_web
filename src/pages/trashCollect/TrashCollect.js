306389cfb8c12e27ca4cb328ad93a2f98a895379
nealdam <neal.dam.1008@gmail.com>
2020-04-04 15:40:42 +0700

    Tab panel

--- src/pages/trashCollect/TrashCollect.js
+++ src/pages/trashCollect/TrashCollect.js
@@ -1,21 +1,22 @@
-import { Button, ButtonGroup } from '@material-ui/core';
+import { Button, ButtonGroup, TabPan, Typography, Box } from '@material-ui/core';
 import React, { useState } from 'react';
 import { collectTrashSteps } from '../../constants/steps';
 import HorizontalLinearStepper from '../../organisms/linearStepper/HorizontalLinearStepper';
 import StreetRouteTable from '../../organisms/streetRouteTable/StreetRouteTable';
-
-
+import PropTypes from 'prop-types';
 
 function createStreetRoute(street, district, city, numberOfRequest) {
     return { street, district, city, numberOfRequest };
 }
 
-export default function TrashCollect() {
+function TrashCollect() {
 
     const [streetRoutes, setStreetRoutes] = useState([
         createStreetRoute('Thành Công', 'Tân Phú', 'Hồ Chí Minh', 3),
         createStreetRoute('Trương Vĩnh Ký', 'Tân Phú', 'Hồ Chí Minh', 10)
     ]);
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = collectTrashSteps;
 
     // useEffect(() => {
     //     console.log("Fetch street route");
@@ -23,9 +24,6 @@ export default function TrashCollect() {
     //     console.log("Result: ");
     //     console.log(streetRoutes);
     // }, [])
-
-    const [activeStep, setActiveStep] = useState(0);
-    const steps = collectTrashSteps;
 
     const handleNextStep = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -44,9 +42,42 @@ export default function TrashCollect() {
                 <Button disabled={activeStep === 0} onClick={handleBackStep}>Back</Button>
                 <Button disabled={activeStep === (steps.length - 1)} onClick={handleNextStep}>Next</Button>
             </ButtonGroup>
-            <StreetRouteTable
-                streetRoutes={streetRoutes}
-            />
+            <TabPanel value={activeStep} index={0}>
+                <StreetRouteTable
+                    streetRoutes={streetRoutes}
+                />
+            </TabPanel>
+            <TabPanel value={activeStep} index={1}>
+                Driver table
+            </TabPanel>
+            <TabPanel value={activeStep} index={2}>
+                Confirm
+            </TabPanel>
         </div>
     );
 }
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+export default TrashCollect;
