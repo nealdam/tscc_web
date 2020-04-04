b56c20cec87fa2ecdec9243835a5dc67917e8620
nealdam <neal.dam.1008@gmail.com>
2020-04-04 15:08:10 +0700

    new table js

--- src/pages/trashCollect/TrashCollect.js
+++ src/pages/trashCollect/TrashCollect.js
@@ -1,55 +1,10 @@
-import { TableContainer, Table, makeStyles, TableBody, TableRow, Checkbox, TableCell, TablePagination, ButtonGroup, Button } from '@material-ui/core';
-import React, { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../context/PageProvider';
-import { getStreetRoute } from '../../services/operatorService';
-import EnhancedTableHead from '../../molecule/enhancedTableHead/EnhancedTableHead';
-import PropTypes from 'prop-types';
-import { streetRouteHeadCells } from '../../constants/headCells';
-import HorizontalLinearStepper from '../../organisms/linearStepper/HorizontalLinearStepper';
+import { Button, ButtonGroup } from '@material-ui/core';
+import React, { useState } from 'react';
 import { collectTrashSteps } from '../../constants/steps';
+import HorizontalLinearStepper from '../../organisms/linearStepper/HorizontalLinearStepper';
+import StreetRouteTable from '../../organisms/streetRouteTable/StreetRouteTable';
 
-const useStyles = makeStyles((theme) => ({
-    table: {
-        minWidth: 750,
-    },
-    visuallyHidden: {
-        border: 0,
-        clip: 'rect(0 0 0 0)',
-        height: 1,
-        margin: -1,
-        overflow: 'hidden',
-        padding: 0,
-        position: 'absolute',
-        top: 20,
-        width: 1,
-    },
-}))
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
 
 function createStreetRoute(street, district, city, numberOfRequest) {
     return { street, district, city, numberOfRequest };
@@ -66,13 +21,6 @@ export default function TrashCollect() {
     //     console.log(streetRoutes);
     // }, [])
 
-    const classes = useStyles();
-    const [order, setOrder] = useState('asc');
-    const [orderBy, setOrderBy] = useState('numOfRequest');
-    const [selected, setSelected] = useState([]);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-
     const [activeStep, setActiveStep] = useState(0);
     const steps = collectTrashSteps;
 
@@ -83,50 +31,6 @@ export default function TrashCollect() {
     const handleBackStep = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     }
-
-    const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const handleSelectAllClick = (event) => {
-        if (event.target.checked) {
-            const newSelecteds = streetRoutes.map((n) => n.name);
-            setSelected(newSelecteds);
-            return;
-        }
-        setSelected([]);
-    }
-
-    const handleClick = (event, name) => {
-        const selectedIndex = selected.indexOf(name);
-        let newSelected = [];
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-
-        setSelected(newSelected);
-    };
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const isSelected = (street) => selected.indexOf(street) !== -1;
-
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, streetRoutes.length - page * rowsPerPage);
-
     return (
         <div>
             <HorizontalLinearStepper
@@ -137,68 +41,8 @@ export default function TrashCollect() {
                 <Button disabled={activeStep === 0} onClick={handleBackStep}>Back</Button>
                 <Button disabled={activeStep === (steps.length - 1)} onClick={handleNextStep}>Next</Button>
             </ButtonGroup>
-            <TableContainer>
-                <Table
-                    className={classes.table}
-                    size={'medium'}
-                >
-                    <EnhancedTableHead
-                        classes={classes}
-                        numSelected={selected.length}
-                        order={order}
-                        orderBy={orderBy}
-                        onSelectAllClick={handleSelectAllClick}
-                        onRequestSort={handleRequestSort}
-                        rowCount={streetRoutes.length}
-                        headCells={streetRouteHeadCells}
-                    />
-                    <TableBody>
-                        {stableSort(streetRoutes, getComparator(order, orderBy))
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row, index) => {
-                                const isItemSelected = isSelected(row.street);
-                                const labelId = `enhanced-table-checkbox-${index}`;
-
-                                return (
-                                    <TableRow
-                                        hover
-                                        onClick={(event) => handleClick(event, row.street)}
-                                        role="checkbox"
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        key={row.street}
-                                        selected={isItemSelected}
-                                    >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox
-                                                checked={isItemSelected}
-                                                inputProps={{ 'aria-labelledby': labelId }}
-                                            />
-                                        </TableCell>
-                                        <TableCell component="th" id={labelId} scope="row" padding="none">
-                                            {row.street}
-                                        </TableCell>
-                                        <TableCell align="left" padding="none">{row.district}</TableCell>
-                                        <TableCell align="left" padding="none">{row.city}</TableCell>
-                                        <TableCell align="right">{row.numberOfRequest}</TableCell>
-                                        <TableCell align="left">Button</TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                        {emptyRows > 0 && (
-                            <TableRow style={{ height: 53 * emptyRows }}>
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                component="div"
-                count={streetRoutes.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onChangePage={handleChangePage}
+            <StreetRouteTable
+                streetRoutes={streetRoutes}
             />
         </div>
     );
