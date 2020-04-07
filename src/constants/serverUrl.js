38a0af14efa77d614dc7565feccc634f65620b3b
nealdam <neal.dam.1008@gmail.com>
2020-04-07 08:11:14 +0700

    hosting, send direction

--- src/constants/serverUrl.js
+++ src/constants/serverUrl.js
@@ -2,3 +2,5 @@ const SERVER_URL = "http://localhost:5000/api";
 
 export const URL_GET_TRASH_AREAS = SERVER_URL + "/trash-areas/temp";
 export const URL_GET_DRIVERS = SERVER_URL + "/employees/drivers";
+
+export const URL_POST_ASSIGN_TRASH = SERVER_URL + "/notification/direction";
