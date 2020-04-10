737facf3d8155cb4f87598989b51c685e874943c
nealdam <neal.dam.1008@gmail.com>
2020-04-10 08:28:14 +0700

    clean up

--- src/services/operatorService.js
+++ src/services/operatorService.js
@@ -1,48 +1,65 @@
-import { getApi, postApi, getImage } from '../utils/apiConsumer';
-import { URL_GET_TRASH_AREAS, URL_GET_DRIVERS, URL_POST_ASSIGN_TRASH, URL_GET_IMAGE } from '../constants/serverUrl';
+import { URL_GET_DRIVERS, URL_GET_IMAGE, URL_GET_TRASH_AREAS, URL_POST_ASSIGN_TRASH } from '../constants/serverUrl';
 
 const axios = require('axios').default;
 
-const getImageConfig = {
+const getImageConfig = (token) => {
+    return {
+        responseType: 'arraybuffer',
+        headers: {
+            Accept: '*/*',
+            Authorization: `Bearer ${token}`
+        },
+        transformResponse: [(data) => {
+            const base64 = btoa(
+                new Uint8Array(data).reduce(
+                    (data, byte) => data + String.fromCharCode(byte),
+                    '',
+                ),
+            );
 
+            const src = `data:;base64,${base64}`;
+
+            return src;
+        }]
+    }
+}
+
+const getConfig = (token) => {
+    return {
+        headers: {
+            Accept: '*/*',
+            Authorization: `Bearer ${token}`
+        }
+    }
+}
+
+const postConfig = (token) => {
+    return {
+        headers: {
+            Accept: '*/*',
+            Authorization: `Bearer ${token}`
+        }
+    }
 }
 
 
 export function getTrashAreas(token) {
-    return getApi(token, URL_GET_TRASH_AREAS);
+    return axios.get(URL_GET_TRASH_AREAS, getConfig(token));
 }
 
 export function getDrivers(token) {
-    return getApi(token, URL_GET_DRIVERS);
+    return axios.get(URL_GET_DRIVERS, getConfig(token));
 }
 
-export function sendDirection(token, body) {
-    return postApi(token, URL_POST_ASSIGN_TRASH, body);
+export function sendDirection(token, data) {
+    return axios.post(URL_POST_ASSIGN_TRASH, data, postConfig(token));
 }
 
 export function getImageAPI(token, listImageNames) {
     let requests = [];
 
     listImageNames.map(imageName => {
-        const request = axios.get(`${URL_GET_IMAGE}${imageName}`, {
-            responseType: 'arraybuffer',
-            headers: {
-                Accept: '*/*',
-                Authorization: `Bearer ${token}`
-            },
-            transformResponse: [(data) => {
-                const base64 = btoa(
-                    new Uint8Array(data).reduce(
-                        (data, byte) => data + String.fromCharCode(byte),
-                        '',
-                    ),
-                );
-
-                const src = `data:;base64,${base64}`;
-
-                return src;
-            }]
-        })
+        const request = axios.get(`${URL_GET_IMAGE}${imageName}`, getImageConfig(token));
 
         requests.push(request);
     });
