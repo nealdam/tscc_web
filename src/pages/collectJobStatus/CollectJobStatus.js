528dc50dfb35290dfa6cfb960c186312bb081f42
nealdam <neal.dam.1008@gmail.com>
2020-04-12 21:09:18 +0700

    collect job grid list

--- src/pages/collectJobStatus/CollectJobStatus.js
+++ src/pages/collectJobStatus/CollectJobStatus.js
@@ -3,11 +3,12 @@ import { getCollectJob } from '../../services/operatorService';
 import { UserContext } from '../../context/PageProvider';
 import { useSnackbar } from 'notistack';
 import { successNotify, errorNotify } from '../../constants/notistackOption';
-import { Card, CardContent, Typography, makeStyles, GridList, CardHeader, Avatar, IconButton, Divider, List, GridListTile } from '@material-ui/core';
+import { Card, CardContent, Typography, makeStyles, GridList, CardHeader, Avatar, IconButton, Divider, List, GridListTile, Button, ListItem, ListItemAvatar, ListItemText, ListItemIcon } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import { isToday } from '../../utils/dateUtil';
 import DepartureBoardIcon from '@material-ui/icons/DepartureBoard';
 import DoneIcon from '@material-ui/icons/Done';
+import MoreIcon from '@material-ui/icons/MoreHoriz';
 
 const useStyles = makeStyles({
     root: {
@@ -35,9 +36,9 @@ function CollectJobStatus() {
 
     const [collectJobs, setCollectJobs] = useState([]);
 
-    useEffect(() => {
-        fetchCollectJob();
-    }, [])
+    // useEffect(() => {
+    //     fetchCollectJob();
+    // }, [])
 
     const getCreateDate = (dateString) => {
         const d = new Date(dateString);
@@ -79,9 +80,59 @@ function CollectJobStatus() {
         )
     }
 
+    const FetchButton = () => {
+        if (collectJobs.length === 0) {
+            return <Button onClick={fetchCollectJob}>Fetch collect job</Button>
+        }
+        return <div></div>
+    }
+
+    const PreViewTrashAreas = (props) => {
+
+        console.log(props);
+
+        const trashAreas = props.trashAreas;
+
+        const items = [];
+        let maxLength = 3;
+
+        if (trashAreas.length < maxLength) {
+            maxLength = trashAreas.length;
+        }
+
+        for (var i = 0; i < maxLength; i++) {
+            items.push(
+                <ListItem key={i}>
+                    <ListItemAvatar>
+                        {getStatus(trashAreas[i].status.name)}
+                    </ListItemAvatar>
+                    <ListItemText primary={trashAreas[i].street} />
+                </ListItem>
+            )
+        }
+
+        if (trashAreas.length > 3) {
+            items.push(
+                <ListItem button>
+                    <ListItemIcon>
+                        <MoreIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="more" />
+                </ListItem>
+            )
+        }
+
+        return (
+            <List>
+                {items}
+            </List>
+        )
+    }
+
     return (
         <div>
-            <GridList cellHeight={300} cols={3} spacing={10}>
+            <FetchButton />
+            <GridList cellHeight={340} cols={3} spacing={10}>
                 {collectJobs.map((collectJob) => (
                     <GridListTile key={collectJob.id} cols={1}>
                         <Card
@@ -90,14 +141,17 @@ function CollectJobStatus() {
                         >
                             <CardHeader
                                 avatar={getStatus(collectJob.status.name)}
+                                action={
+                                    <IconButton>
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
                                 title={collectJob.driver ? `Driver: ${collectJob.driver.name}` : "Driver Name"}
                                 subheader={collectJob.createAt && `Assign at: ${getCreateDate(collectJob.createAt)}`}
                             />
                             <Divider />
                             <CardContent>
-                                <List>
-
-                                </List>
+                                <PreViewTrashAreas trashAreas={collectJob.trashAreas} />
                             </CardContent>
                         </Card>
                     </GridListTile>
